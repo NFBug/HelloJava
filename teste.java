b8a3ea567110e37b5ea675ec56cc697e813e8664
NFBug <99752258+NFBug@users.noreply.github.com>
2023-05-21 20:14:14 -0300

    Add files via upload

---
+++ teste.java
@@ -0,0 +1,11 @@
+
+public class teste{
+    public static void main(String[] args){
+        DIST.log("Hello");
+    }
+}    
+class DIST{
+    public static void log(Object con) {
+        System.out.println(con);
+    }
+}
